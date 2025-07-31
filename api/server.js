3483ceba37d6f244d4ad03f0d874d098f4cde7e3
V0000DY <proektant.burenie@gmail.com>
2025-07-31 20:48:06 +0700

    change router in server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
-const fs = require("fs");
 const jsonServer = require("json-server");
-const path = require("path");
-
 const server = jsonServer.create();
 
-const router = jsonServer.router(path.resolve(__dirname, "db.json"));
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+
+const router = jsonServer.router(db);
 
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
